98e3fb2961b696dfc25703db27bd736f2dd1df35
tangtao <412145540@qq.com>
2016-07-26 00:17:11 +0800

    init

--- main.go
+++ main.go
@@ -3,7 +3,66 @@ package main
 import (
 	"gitlab.qiyunxin.com/tangtao/utils/startup"
 	"gitlab.qiyunxin.com/tangtao/utils/config"
+	. "gitlab.qiyunxin.com/tangtao/utils/route"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+	"log"
 )
+
+type LogHandler struct  {
+
+}
+
+type AllowOrigin struct {
+
+}
+
+func (self AllowOrigin)  Handler(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin","*");
+		w.Header().Set("Access-Control-Allow-Headers","*");
+		inner.ServeHTTP(w, r)
+	})
+}
+
+func (self LogHandler)  Handler(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log.Printf(
+			"%s\t%s\t%s\t%s",
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
+}
+
+
+func GetRouters()  *mux.Router{
+
+	//日志处理
+	var logHander IHandler  =LogHandler{}
+	//跨域处理
+	var allowOrigin IHandler = AllowOrigin{}
+
+	return  NewRouterWithHandle([]Route{
+
+		Route{  //应用申请
+			"SubmitApp",
+			"POST",
+			"/pay/app",
+			action.SubmitApp,
+		},
+
+
+	},[]IHandler{logHander,allowOrigin})
+}
+
 
 func main() {
 	//os.Setenv("APPID","commuser")
---
+++ vendor/api/api.go
@@ -0,0 +1,8 @@
+package api
+
+import "net/http"
+
+//登录
+func Login(w http.ResponseWriter, r *http.Request)  {
+
+}
