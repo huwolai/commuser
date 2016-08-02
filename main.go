616471cb5565df4a9dae08344648832535691e43
tangtao <412145540@qq.com>
2016-08-02 14:17:33 +0800

    init

--- main.go
+++ main.go
@@ -9,6 +9,21 @@ import (
 )
 
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
 	//os.Setenv("APPID","commuser")
@@ -29,6 +44,8 @@ func main() {
 	}
 
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 
 	router.POST("/login",api.Login)
 
