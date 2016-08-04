369b3cb8b9557e081b47579e98fa39accf018407
tangtao <412145540@qq.com>
2016-08-04 18:01:02 +0800

    init

--- config/sql/init.sql
+++ config/sql/init.sql
@@ -1,16 +1,4 @@
 -- +migrate Up
--- 应用表
-CREATE TABLE IF NOT EXISTS app(
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  app_id VARCHAR(255) UNIQUE COMMENT '应用ID',
-  app_key VARCHAR(255) COMMENT '应用KEY',
-  app_name VARCHAR(255) COMMENT '应用名称',
-  app_desc VARCHAR(1000) COMMENT '应用描述',
-  status int COMMENT '应用状态 0.待审核 1.已审核',
-  create_time TIMESTAMP COMMENT '创建时间',
-  update_time TIMESTAMP COMMENT '更新时间'
-
-) CHARACTER SET utf8;
 
 -- 用户表
 CREATE TABLE IF NOT EXISTS user(
