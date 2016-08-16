5299133f12cfaf9d56ed5387fa1577a44a06cde3
tangtao <412145540@qq.com>
2016-08-16 14:43:58 +0800

    init

--- vendor/service/sms.go
+++ vendor/service/sms.go
@@ -18,12 +18,17 @@ const SMS_BASE_URL  = "https://app.cloopen.com:8883"
 //发送验证码短信
 func SendCodeSMS(mobile string,code string) (error) {
 
+
+	accountSid := "aaf98f8950f4a62c0151061744fd43b8"
+	accountToken :="834b696ff4954450b751a8a4590f1fe0"
+	appId :="aaf98f89512446e2015128f0802e0d4e"
 	date  :=time.Now()
 	dateStr :=date.Format("20060102150405")
+	log.Info(dateStr)
 
-	auth :=base64.StdEncoding.EncodeToString([]byte("aaf98f8950f4a62c0151061744fd43b8,"+dateStr))
+	auth :=base64.StdEncoding.EncodeToString([]byte(accountSid+","+dateStr))
 	h:= md5.New()
-	h.Write([]byte("aaf98f8950f4a62c0151061744fd43b8"+"834b696ff4954450b751a8a4590f1fe0"+dateStr))
+	h.Write([]byte(accountSid+accountToken+dateStr))
 	sign :=h.Sum(nil)
 	signStr :=hex.EncodeToString(sign)
 	header :=map[string]string{
@@ -34,7 +39,7 @@ func SendCodeSMS(mobile string,code string) (error) {
 
 	param :=map[string]interface{}{
 		"to":mobile,
-		"appId":"aaf98f89512446e2015128f0802e0d4e",
+		"appId":appId,
 		"templateId":"51954",
 		"datas":[]string{code,"企云信"},
 	}
@@ -42,7 +47,7 @@ func SendCodeSMS(mobile string,code string) (error) {
 
 
 	jsonData,_ := json.Marshal(param)
-	 resopnse,err :=network.Post(SMS_BASE_URL+"/2013-12-26/Accounts/aaf98f8950f4a62c0151061744fd43b8/SMS/TemplateSMS?sig="+signStr,jsonData,header)
+	 resopnse,err :=network.Post(SMS_BASE_URL+"/2013-12-26/Accounts/"+accountSid+"/SMS/TemplateSMS?sig="+signStr,jsonData,header)
 	if err!=nil {
 
 		return err
